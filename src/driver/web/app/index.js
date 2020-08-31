00159d63fdc3dcdf75ec4d9588484192be2a562e
Nve Hounkponou Orphée <norpheehounkponou@gmail.com>
2020-08-31 01:18:00 +0100

    feat redirect

--- src/driver/web/app/index.js
+++ src/driver/web/app/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const landing_page = require('./landing_page');
+const prestataire = require('./prestataire');
+const donneur_dordre = require('./donneur_dordre');
 
 module.exports = (app) => {
 
   app.use(express.static(__dirname + '/public'));
   
   app.use(landing_page);
+  app.use('/prestataire', prestataire);
+  app.use('/donneur-dordre', donneur_dordre);
 
   app.set('view engine', 'pug');
   app.set('views', __dirname + '/views');
